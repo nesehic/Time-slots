6b822646f74cc036aa36c77c117d1a4eb73bc056
nesehic <nedim@stagecoding.com>
2023-06-27 16:52:11 +0200

    Edge cases & cleanup

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,13 +1,35 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
-final format = DateFormat('hh:mm');
+final formatHHmm = DateFormat('HH:mm');
 
 class HomeController extends GetxController {
-  final position = Offset.zero.obs;
-  final scroll = ScrollController();
+  final fifteenHeight = 60.0; // Must be divisible by 3 and 2
+  late final fiveHeight = fifteenHeight / 3.0;
+  late final fourtyFiveHeight = fifteenHeight * 3.0;
 
-  // Format from - to
-  String selectedTime = "";
+  final firstPositionY = 0.0;
+  final position = 0.0.obs;
+  String text = '00:00 - 00:45';
+
+  List<DateTime> getTimesFromMiddle(DateTime time) {
+    final start = time.add(const Duration(days: 1, minutes: -20));
+    final end = start.add(const Duration(minutes: 45));
+
+    // Edge cases
+    if (start.day == 1) {
+      return [formatHHmm.parse('00:00'), formatHHmm.parse('00:45')];
+    }
+    if (end.hour == 23 && end.minute > 25) {
+      return [formatHHmm.parse('23:00'), formatHHmm.parse('23:45')];
+    }
+
+    return [start, end];
+  }
+
+  double getPositionFromTime(DateTime start) {
+    final minutes = start.hour * 60 + start.minute;
+    final index = minutes / 5;
+    return fiveHeight * index + fifteenHeight / 2.0;
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,83 +1,66 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
 
-  // This is 15 minute height.
-  static const fifteenHeight = 50.0;
-
   @override
   Widget build(BuildContext context) {
     final initial = DateTime(0);
     return Scaffold(
       body: SafeArea(
-        child: SizedBox(
-          width: double.infinity,
-          child: SingleChildScrollView(
-            controller: controller.scroll,
-            child: Stack(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    for (int i = 0; i < 96; i++)
-                      _buildTime(
-                        format.format(initial.add(Duration(minutes: 15 * i))),
-                      ),
-                  ],
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  for (int i = 0; i < 96; i++)
+                    _buildTime(initial.add(Duration(minutes: 15 * i))),
+                ],
+              ),
+              Obx(
+                () => Container(
+                  transform: Matrix4.identity()
+                    ..setTranslationRaw(
+                      0.0,
+                      controller.firstPositionY + controller.position.value,
+                      0.0,
+                    ),
+                  margin: const EdgeInsets.only(left: 50.0),
+                  width: double.infinity,
+                  height: controller.fourtyFiveHeight,
+                  color: Colors.amber,
+                  child: Text(controller.text),
                 ),
-                Obx(
-                  () => Container(
-                    transform: Matrix4.identity()
-                      ..setTranslationRaw(
-                        50.0,
-                        controller.position.value.dy,
-                        0.0,
-                      ),
-                    width: 100.0,
-                    height: fifteenHeight * 3,
-                    color: Colors.amber,
-                    child: Text(controller.selectedTime),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 
-  String getTimeFromMiddle(String stime) {
-    final time = format.parse(stime);
-    final start = time.subtract(const Duration(minutes: 20));
-    final end = time.add(const Duration(minutes: 25));
-    return '${format.format(start)} - ${format.format(end)}';
-  }
-
-  Widget _buildTime(String time) {
+  Widget _buildTime(DateTime time) {
     return GestureDetector(
-      onTapDown: (details) {
-        controller.selectedTime = getTimeFromMiddle(time);
-        controller.position.value = details.globalPosition;
-        controller.position.value = Offset(
-          controller.position.value.dx,
-          controller.position.value.dy +
-              controller.scroll.position.pixels -
-              details.localPosition.dy -
-              fifteenHeight,
+      onTap: () {
+        final times = controller.getTimesFromMiddle(time);
+        controller.text = times.map((e) => formatHHmm.format(e)).join(' - ');
+        controller.position.value = controller.getPositionFromTime(
+          times.first,
         );
       },
       child: Container(
         color: Colors.transparent,
-        height: fifteenHeight,
+        height: controller.fifteenHeight,
         child: Row(
-          children: [Text(time), const Expanded(child: Divider())],
+          children: [
+            Text(formatHHmm.format(time)),
+            const Expanded(child: Divider()),
+          ],
         ),
       ),
     );
